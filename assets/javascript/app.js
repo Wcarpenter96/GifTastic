f792fb72118ae7aa5e00004a1ef4b4b62e179b2a
Weston Carpenter <wc.cc96@gmail.com>
2019-09-09 21:45:18 -0700

    gif stills displayed on button click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,30 @@
 // Initial array of shows
-shows = ["Friends", "Game of Thrones", "Modern Family", "Psych", "Beat Bobby Flay"];
+shows = ["Friends", "Game of Thrones", "Modern Family", "Psych", "The Office"];
+var numGifs = 10;
 
 // displayGifs function displays "numGifs" amount of "show" gifs with their respective ratings
 function displayGifs() {
 
     var show = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=HRrMbCNwCHClP2xPAixFjjvPSkGL784T"
-
+    var queryURL = `http://api.giphy.com/v1/gifs/search?q=${show}&api_key=HRrMbCNwCHClP2xPAixFjjvPSkGL784T&limit=${numGifs}`
     $.ajax({
         url: queryURL,
         method: "GET"
     }).then(function (response) {
         console.log(response);
+        response.data.forEach(gif => {
+            $gif = $(`<img src="${gif.images.original_still.url}">`);
+            $gif.addClass('gif');
+            $('#gifs-view').append($gif);
+        });
     });
 
 }
+
+function playGif(){
+    console.log('play');
+}
+
 
 // Function for re-rendering default and user-initialized buttons
 function renderButtons() {
@@ -36,6 +46,8 @@ $("#add-show").on("click", function(event) {
     renderButtons();
 });
 
+
 $(document).on("click", ".show", displayGifs);
+$(document).on("click", ".gif", playGif);
 renderButtons();
 
